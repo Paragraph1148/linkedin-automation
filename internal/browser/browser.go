e7bbba7e9878322629980112c2041d9a37d6b28f
Paragraph1148 <actuallyamountain@proton.me>
2025-12-19 16:04:48 +0530

    browser.go

---
+++ internal/browser/browser.go
@@ -0,0 +1,26 @@
+package browser
+
+import (
+	"log"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/lib/launcher"
+)
+
+func LaunchBrowser() (*rod.Browser, error) {
+	log.Println("Launching Rod-managed Chromium (headless, no sandbox)")
+
+	u := launcher.New().
+		Headless(true).
+		NoSandbox(true).
+		Leakless(false).
+		MustLaunch()
+
+	browser := rod.New().ControlURL(u)
+
+	if err := browser.Connect(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Chromium launched successfully")
+	return browser, nil
+}
